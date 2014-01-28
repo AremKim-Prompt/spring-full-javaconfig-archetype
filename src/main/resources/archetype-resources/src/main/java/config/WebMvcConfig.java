a79f8f49e9f45cd26e22c40324dd0b787878c3b8
Madis Maask <madismaask@gmail.com>
2014-01-28 23:41:14 +0200

    change messagesource

--- src/main/resources/archetype-resources/src/main/java/config/WebMvcConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/WebMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -19,14 +19,14 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @ComponentScan(basePackages = "${package}", useDefaultFilters = true)
 class WebMvcConfig extends WebMvcConfigurationSupport {
 
-	private static final String MESSAGE_SOURCE_BASE = "WEB-INF/classes/messages";
+	private static final String MESSAGE_SOURCE_BASE = "messages";
 
 	private static final String RESOURCES_HANDLER = "/resources/";
 	private static final String RESOURCES_LOCATION = RESOURCES_HANDLER + "**";
 
 	@Bean
 	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename(MESSAGE_SOURCE_BASE);
 		messageSource.setUseCodeAsDefaultMessage(true);
 		messageSource.setCacheSeconds(5);
